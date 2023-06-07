946320df96bcd6acd0f106f3acd703102042f575
Farkhadov95 <farkhadov950405@gmail.com>
2023-06-07 23:53:52 +0100

    Hw. Created mock data

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const commentTexts = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const commentAuthors = [
+  'Masha', 'Petya', 'Vasya', 'Amir', 'Sasha', 'Misha', 'Abbos', 'Jasur', 'Vova', 'Sergey', 'Vladimir'
+];
+
+const generateComments = (number) => {
+  const comments = [];
+  for (let i = 0; i < number; i++) {
+    comments.push({
+      id: i,
+      avatar: `img/avatar-${Math.floor(Math.random() * 6) + 1}.svg`,
+      message: commentTexts[Math.floor(Math.random() * commentTexts.length)],
+      name: commentAuthors[Math.floor(Math.random() * commentAuthors.length)]
+    });
+  }
+  return comments;
+};
+
+const generatePosts = (number) => {
+  const posts = [];
+  for (let i = 0; i < number; i++) {
+    posts.push({
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: `nice photo number ${i}`,
+      likes: Math.floor(Math.random() * 186) + 15,
+      comments: generateComments(Math.floor(Math.random() * 30))
+    });
+  }
+
+  return posts;
+};
+
+console.log(generatePosts(35));
