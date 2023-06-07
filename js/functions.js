647b878bf2b50eeefc26530e00342fd71739ab56
Farkhadov95 <farkhadov950405@gmail.com>
2023-06-07 16:16:05 +0100

    HW. Functions"

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+function isMatching(str, num) {
+  return str.length === num ? console.log('true') : console.log('false');
+}
+isMatching('hello', 6);
+
+function isPalindrome(str) {
+  return str.toUpperCase() === str.toUpperCase().split('').reverse().join('') ? console.log('true') : console.log('false');
+}
+isPalindrome('tenet');
+
+function numbersSeparator(str) {
+  const numbersArray = str.match(/\d+/g);
+  return numbersArray === null ? console.log('NaN') : console.log(numbersArray.join(''));
+}
+
+numbersSeparator('This1 is b3ers1');
